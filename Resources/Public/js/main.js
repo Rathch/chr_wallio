3578d0a2071816f3f46e8f64f237026c8dbbf6cb
Christian Rath <christian.rath@publicispixelpark.de>
2019-09-09 12:08:19 +0200

    [TASK] add css, js ant layout

---
+++ Resources/Public/js/main.js
@@ -0,0 +1,7 @@
+function addWallio(wall_url) {
+    $('#wallio-gdpr-notice').hide();
+    $('#wallio-container').html('<script id="wallsio-widget-script" src="https://walls.io/js/wallsio-widget-1.2.js" data-wallurl="'+wall_url+'?nobackground=1&amp;show_header=0&amp;initial_posts=8" data-width="100%" data-autoheight="1" data-height="800" data-lazyload="1"></script>');
+}
+
+
+
